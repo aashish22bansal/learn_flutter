98731b8c82884be446435080e21d2a4b5a0433f4
Aashish Bansal <aashish22bansal@gmail.com>
2022-12-21 18:07:41 +0530

    Columns Widget
    Columns Widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,55 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favourite color?',
+      'What is your favourite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Column(children: <Widget>[], ),
+        body: Column(
+          children: <Widget>[
+            Text(
+              questions.elementAt(questionIndex),
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+            Text(
+              questions.elementAt(questionIndex),
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ), // Render Widgets in a Column
       ),
     );
   }
-} 
+}
